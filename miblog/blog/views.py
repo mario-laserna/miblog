e0a7e87123aa57b8823ba02eaaffdaeebccf0c08
mario <laserna.mario@gmail.com>
2014-06-19 21:52:49 -0500

    vista para ver lista de entradas

--- miblog/blog/views.py
+++ miblog/blog/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
+from django.core.urlresolvers import reverse
+import time
+from calendar import month_name
 
-# Create your views here.
+from blog.models import *
+
+def mkmonth_lst():
+	if not Entrada.objects.count() : return []
+
+	year, month = time.localtime()[:2]
+	first = Entrada.objects.order_by("fecha")[0]
+	fyear = first.fecha.year
+	fmonth = first.fecha.month
+	months = []
+
+	for y in range(year, fyear-1, -1):
+		start, end = 12,0
+		if y == year:start = month
+		if y == fyear: end = fmonth-1
+
+		for m in range(start, end, -1):
+			months.append((y,m,month_name[m]))
+
+	return months
+
+
+def month(request, year, month):
+	entrada = Entrada.objects.filter(fecha__year=year, fecha__month=month)
+	return render_to_response("listado.html", dict(entrada_list=entrada, user=request.user, month=mkmonth_lst(), archive=True))
+
+
+def entrada(request, pk):
+	identrada = Entrada.objects.get(pk=int(pk))
+	return render_to_response("entrada.html", dict(entrada=identrada, usuario=request.user))
+
+
+def main(request):
+	entrada = Entrada.objects.all().order_by("-fecha")
+	paginator = Paginator(entrada, 3)
+
+	try: 
+		pagina = int(request.GET.get("page", 1))
+	except ValueError:
+		pagina = 1
+
+	try:
+		entrada = paginator.page(pagina)
+	except (InvalidPage, EmptyPage):
+		entrada = paginator.page(paginator.num_pages)
+
+	return render_to_response("listado.html", dict(entrada = entrada, usuario = request.user, entrada_list = entrada.object_list, months=mkmonth_lst()))
--- miblog/miblog/urls.py
+++ miblog/miblog/urls.py
@@ -8,5 +8,11 @@ urlpatterns = patterns('',
     # url(r'^$', 'miblog.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns = patterns('blog.views',
+	(r"^entrada/(?P<pk>\d+)/$", "entrada"),
+	(r"^month/(\d+)/(\d+)/$", "month"),
+	(r"", "main"),
 )
